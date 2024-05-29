d6534f2685ce502725e5522fdc85221cfaaf77a8
RaghulSelvaraj25 <166625027+RaghulSelvaraj25@users.noreply.github.com>
2024-05-30 01:06:00 +0530

    WEB GUI
    I had completed my project Simple Interest Calculator by using HTML,CSS,JAVA SCRIPT which was performed by dynamivcally.

---
+++ simple interest script.js
@@ -0,0 +1,20 @@
+let calculatePress = document.getElementById("button-btn");
+let result = document.getElementById("result");
+let calculate = () => {
+  let p = Number(document.getElementById("principal").value);
+  let r = Number(document.getElementById("rate").value);
+  let t = Number(document.getElementById("time").value);
+  let duration = document.getElementById("duration").value;
+  let simpleInterest =
+    duration == "year" ? (p * r * t) / 100 : (p * r * t) / 1200;
+  let amount = p + simpleInterest;
+
+  result.innerHTML = `<div>Principal Amount: <span>${p.toFixed(2)}</span></div>
+  <div>Total Interest: <span>${simpleInterest.toFixed(2)}</span></div>
+  <div>Total Amount: <span>${amount.toFixed(2)}</span></div>`;
+};
+calculatePress.addEventListener("click", calculate);
+window.addEventListener("load", calculate);
+   
+   
+
